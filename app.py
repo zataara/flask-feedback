a82249afb32f36993f6a134e31c0eadb58c20ca1
zataara <bryantmac@gmail.com>
2021-07-22 10:44:35 -0700

    updated db name in psql

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
-from secrets import API_SECRET_KEY
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'supersecretkey'
