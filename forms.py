5deead1f0b8e85e0cc0d2f1d9201e018ec953d39
zataara <bryantmac@gmail.com>
2021-07-22 11:56:13 -0700

    added class for UserForm

--- forms.py
+++ forms.py
@@ -1,3 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, SelectField, IntegerField, BooleanField
 from wtforms.validators import InputRequired, Optional, Length, NumberRange, URL
+
+
+
+class UserForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(max=20)])
+    password = StringField('Password', validators=[InputRequired()])
+    email = StringField('Email Address', validators=[InputRequired(), Length(max=50)])
+    first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
+    last_name = StringField('Last Name', validators=[InputRequired(), Length(max=30)])
