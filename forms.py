c7ec5bc184634a4fea91917ff192c3c336240ec3
zataara <bryantmac@gmail.com>
2021-07-23 10:37:52 -0700

    added LoginForm for authenticting users

--- forms.py
+++ forms.py
@@ -11,3 +11,8 @@ class UserForm(FlaskForm):
     email = StringField('Email Address', validators=[InputRequired(), Length(max=50)])
     first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
     last_name = StringField('Last Name', validators=[InputRequired(), Length(max=30)])
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(max=20)])
+    password = PasswordField('Password', validators=[InputRequired()])
